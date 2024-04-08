2227c88a0e8a06c57760200e0ea961272485d5fd
vizels <mirchuk.georgiy@gmail.com>
2024-04-08 18:43:16 +0200

    minor changes

--- main.py
+++ main.py
@@ -2,13 +2,10 @@ HEADER_START = 0x00
 
 with open("image.png", "rb") as image:
     file = image.read()
-    f = bytearray(file)
-    print(f[0:10])
+    data = bytearray(file)
+    print(data[0:10])
 
-    f[0] = 0xAA
-    print(f[0:10])
-    
-#   b = bytearray(f)
-#   print b[0]
+    data[0] = 0xAA
+    print(data[0:10])
 
 
