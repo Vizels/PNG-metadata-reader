0a09d54725e7c248cba6d586320917a98ce1f9d4
vizels <mirchuk.georgiy@gmail.com>
2024-04-08 19:21:19 +0200

    Header start and end added as constants

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-HEADER_START = 0x00
+HEADER_START = 0
+HEADER_END = 8
 
 with open("image.png", "rb") as image:
     file = image.read()
     data = bytearray(file)
-    print(data[0:10])
+    print(data[HEADER_START:HEADER_END])
 
     data[0] = 0xAA
-    print(data[0:10])
+    print(data[HEADER_START:HEADER_END])
 
 
