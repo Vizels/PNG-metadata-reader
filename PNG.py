84d5e6523f178e79b23b6bdc3f142d7e7e247059
acodered <alexkondratru@gmail.com>
2024-04-22 21:13:56 +0200

    Move code from with clause

--- PNG.py
+++ PNG.py
@@ -11,23 +11,23 @@ class PNG:
     def parse(self):
         with open(self.file, "rb") as image:
             data = bytearray(image.read())
-            header = data[self.HEADER_START:self.HEADER_END]
-            print(header)
+        header = data[self.HEADER_START:self.HEADER_END]
+        print(header)
 
-            i = self.HEADER_END
+        i = self.HEADER_END
 
-            while i < len(data):
-                size = int.from_bytes(data[i:i+4], byteorder="big")
-                name = data[i+4:i+8].decode("utf-8")
-                chunk_data = data[i+8:i+8+size]
-                crc = data[i+8+size:i+12+size]
-                
-                if name == "IHDR":
-                    chunk = chunks.IHDR(name, size, chunk_data, crc)
-                else:
-                    chunk = chunks.Chunk(name, size, chunk_data, crc)
-                
-                self.chunks.append(chunk)
-                i += size + 12
+        while i < len(data):
+            size = int.from_bytes(data[i:i+4], byteorder="big")
+            name = data[i+4:i+8].decode("utf-8")
+            chunk_data = data[i+8:i+8+size]
+            crc = data[i+8+size:i+12+size]
             
-            print(self.chunks)
+            if name == "IHDR":
+                chunk = chunks.IHDR(name, size, chunk_data, crc)
+            else:
+                chunk = chunks.Chunk(name, size, chunk_data, crc)
+            
+            self.chunks.append(chunk)
+            i += size + 12
+        
+        print(self.chunks)
