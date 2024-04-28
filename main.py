6c9e502a39c915d84c07ef55bfa4757518dfbe22
acodered <alexkondratru@gmail.com>
2024-04-28 14:53:59 +0200

    Add image8

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from PNG import PNG
 
-test = PNG("images/image7.png")
+test = PNG("images/image8.png")
 test.mergeIDAT()
 test.printData()
 test.showPLTE()
-test.fourierTransform()
-# test.saveFile("images/test.png")    
+test.fourierTransform()   
